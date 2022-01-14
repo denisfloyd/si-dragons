58bb51f0bc869f1ee5e7e126b67d6206ab39140a
denismend <denisladeira1@gmail.com>
2022-01-14 11:08:07 -0300

    build: :wrench: add cors on api routes

--- src/pages/api/users/sessions.ts
+++ src/pages/api/users/sessions.ts
@@ -1,13 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from 'cors';
 import jwt from "jsonwebtoken";
+
+import initMiddleware from '@/utils/initMiddlewareApiRoutes';
+
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
 
 interface User {
   username: string;
   password: string;
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Run cors
+  await cors(req, res)
+
   const user = req.body as User;
 
   if (user.username === "admin" && user.password === "admin") {
